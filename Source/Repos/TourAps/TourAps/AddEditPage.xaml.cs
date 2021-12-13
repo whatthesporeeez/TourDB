94d5036b14f209c68df37dd9e952e373b6b29d71
whatthesporeeez <blathzera@yandex.ru>
2021-12-13 17:09:45 +0300

    CommitZvarovscky

---
+++ Source/Repos/TourAps/TourAps/AddEditPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourAps
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+            if(selectedHotel != null)
+                _currentHotel = selectedHotel;
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = TourAgentDBEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors =  new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if (_currentHotel.CountOfStars < 1 || _currentHotel.CountOfStars > 5)
+                errors.AppendLine("Количество звезд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберете страну!");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentHotel.HotelID == 0)
+                TourAgentDBEntities.GetContext().Hotel.Add(_currentHotel);
+            try
+            {
+                TourAgentDBEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack(); 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
