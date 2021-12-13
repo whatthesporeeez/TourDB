9a3f348331bb0f3b1201dcb21f1e357bd628eada
whatthesporeeez <blathzera@yandex.ru>
2021-12-13 17:09:45 +0300

    да

---
+++ Source/Repos/TourAps/TourAps/HotelsPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourAps
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+            //DGridHotels.ItemsSource = TourAgentDBEntities.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null)); 
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    TourAgentDBEntities.GetContext().Hotel.RemoveRange(hotelForRemoving);
+                    TourAgentDBEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+                    DGridHotels.ItemsSource = TourAgentDBEntities.GetContext().Hotel.ToList();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                TourAgentDBEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = TourAgentDBEntities.GetContext().Hotel.ToList(); 
+            }
+        }
+    }
+}
